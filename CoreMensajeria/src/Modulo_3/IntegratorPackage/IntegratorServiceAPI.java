dc67a4e901cc85d988455a7738d0b56c8a0f4429
Unknown <jafjdev@gmail.com>
2018-10-23 19:06:33 +0200

    Integrator List done
    Ya se retorna la lista de integradores desde la base de datos y se transforma a JSON

--- CoreMensajeria/src/Modulo_3/IntegratorPackage/IntegratorServiceAPI.java
+++ CoreMensajeria/src/Modulo_3/IntegratorPackage/IntegratorServiceAPI.java
@@ -1,10 +1,10 @@
 package Modulo_3.IntegratorPackage;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import java.awt.*;
+import javax.ws.rs.core.Response;
+import java.util.List;
 
 @Path("/integrators")
 
@@ -12,7 +12,11 @@ public class IntegratorServiceAPI {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public String listIntegrator() {
-        return IntegratorService.getInstance().listIntegrator().toString();
+    public Response listIntegrator() {
+        List<Integrator> i = IntegratorService.getInstance().listIntegrator();
+        return Response
+                .ok()
+                .entity(i)
+                .build();
     }
 }
