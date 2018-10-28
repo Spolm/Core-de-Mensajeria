aba7ebb496c4c7e5e8843b85959f9cfaeb9ba727
rigojr <rigojunnior@gmail.com>
2018-10-28 16:48:00 -0400

    Creacion base de los modulos de integradores y canales
    Solo se crearon los modulos y los componentes, tambien se modifico el sidebar principal para poder tener navegacion

---
+++ CoreMensajeriaFront/src/app/layout/integrator/integrator.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-integrator',
+  templateUrl: './integrator.component.html',
+  styleUrls: ['./integrator.component.scss']
+})
+export class IntegratorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
