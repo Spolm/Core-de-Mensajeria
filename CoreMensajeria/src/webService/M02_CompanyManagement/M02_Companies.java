0db67f9ea5c8e0c2089e413d54b70ef6682d57d7
Unknown <edwarddenobrega@gmail.com>
2018-10-29 11:10:57 -0400

    Subiendo clases M9

--- CoreMensajeria/src/webService/M02_CompanyManagement/M02_Companies.java
+++ CoreMensajeria/src/webService/M02_CompanyManagement/M02_Companies.java
@@ -4,7 +4,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 import java.sql.*;
-import com.google.gson.Gson;
+//import com.google.gson.Gson;
 
 import Classes.Company;
 import Classes.Sql;
