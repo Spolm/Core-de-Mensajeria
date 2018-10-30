c025ed88be49c35fd8b62217624cd1aec6428b34
Michele <michele_lanza1@hotmail.com>
2018-10-29 21:33:01 -0400

    Añadiendo plotly.js y gráfica de prueba  con datos estáticos

--- CoreMensajeriaFront/src/app/layout/statistics/statistics.component.ts
+++ CoreMensajeriaFront/src/app/layout/statistics/statistics.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./statistics.component.scss']
 })
 export class StatisticsComponent implements OnInit {
-
+ 
+ 
+  public graph = {
+    data: [
+                { x: ['SMS', 'Email'], y: [400, 100], type: 'bar' },
+    ],
+    layout: {width: 500, height: 300, title: 'Cantidad de mensajes enviados por canal'}
+};
+  
   constructor() { }
 
   ngOnInit() {
   }
-
+  
 }
--- CoreMensajeriaFront/src/app/layout/statistics/statistics.module.ts
+++ CoreMensajeriaFront/src/app/layout/statistics/statistics.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StatisticsComponent } from './statistics.component';
 import {StatisticsRoutingModule} from './statistics-routing.module'
+import { PlotlyModule } from 'angular-plotly.js';
+
+
 
 @NgModule({
   imports: [
-    CommonModule, StatisticsRoutingModule
+    CommonModule, StatisticsRoutingModule, PlotlyModule
   ],
   declarations: [StatisticsComponent]
 })
