3387541f1c764be8bd14e1eed5bb865100952bdf
Spolm <Spooner_lm@hotmail.com>
2018-10-31 14:48:45 -0400

    Subiendo Scripts para crear la BD

---
+++ SCRIPTS/CREATE DB.sql
@@ -0,0 +1,16 @@
+-- Database: CoreMensajeria
+
+-- DROP DATABASE "CoreMensajeria";
+
+CREATE DATABASE "CoreMensajeria"
+    WITH 
+    OWNER = "CoreMensajeria"
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_United States.1252'
+    LC_CTYPE = 'English_United States.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+
+    -- Table: public.company
+
+-- DROP TABLE public.company;
---
+++ SCRIPTS/CREATE USER.sql
@@ -0,0 +1,11 @@
+-- User: "CoreMensajeria"
+-- DROP USER "CoreMensajeria";
+
+CREATE USER "CoreMensajeria" WITH
+  LOGIN
+  SUPERUSER
+  INHERIT
+  CREATEDB
+  CREATEROLE
+  NOREPLICATION;
+
