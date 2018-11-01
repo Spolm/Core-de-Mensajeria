66e814a752602998d003f160f01678a68369b9b3
vahevia <heviaveronica@gmail.com>
2018-11-01 16:28:37 -0400

    script de tabla e insert de usuario creados

--- SCRIPTS/CREATES.sql
+++ SCRIPTS/CREATES.sql
@@ -1,11 +1,14 @@
-create table User{
- use_type number;
- use_name varchar(20);
- use_email varchar(30);
- use_phone varchar(20);
- use_country varchar(20);
- use_city varchar(20);
- use_address varchar(20);
- use_genre char;
- use_dayOfBirth timestamp;
-}
+create table public.User(
+ userId serial primary key,
+ userPassword varchar (18),
+ userUsername varchar(20),
+ userType integer,
+ userEmail varchar(30),
+ userPhone varchar(20),
+ userCountry varchar(20),
+ userCity varchar(20),
+ userAddress varchar(20),
+ userDateOfBirth timestamp,
+ userGender char
+);
+
