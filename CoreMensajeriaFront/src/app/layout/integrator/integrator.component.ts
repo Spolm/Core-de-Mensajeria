f286accc7e25d4d1f99253706a6e332837df7269
rigojr <rigojunnior@gmail.com>
2018-11-04 10:23:03 -0400

    Conexión de Backend con Frontend, solo mostrando integradores

--- CoreMensajeriaFront/src/app/layout/integrator/integrator.component.ts
+++ CoreMensajeriaFront/src/app/layout/integrator/integrator.component.ts
@@ -1,19 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-integrator',
   templateUrl: './integrator.component.html',
   styleUrls: ['./integrator.component.scss']
 })
+
 export class IntegratorComponent implements OnInit {
 
   // PRUEBA
-  integrators = ['AWeber','MailChimp','Digitel','Movistar','Movilnet']
+  integrators = [
+    {
+      apiIntegrator: '',
+      idIntegrator: '',
+      messageCost: '',
+      nameIntegrator: '',
+      threadCapacity: ''
+    }
+  ];
   // FIN PRUEBA
   
-  constructor() { }
+  constructor( private http: Http) { 
+
+  }
 
   ngOnInit() {
+    this.http.get( 'http://localhost:8080/CoreMensajeria_war_exploded/' + 'integrators' ).
+      toPromise().then( r => r.json() ).
+      then( r => this.integrators = r);
   }
 
 }
