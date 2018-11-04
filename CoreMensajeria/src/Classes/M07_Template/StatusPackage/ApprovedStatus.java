555321cdf3fe92de8d715f7bac9ea4a69f49b0ec
fabian graterol <fabiancgraterol@gmail.com>
2018-11-04 12:25:03 -0400

    solucion de errores

--- CoreMensajeria/src/Classes/M07_Template/StatusPackage/ApprovedStatus.java
+++ CoreMensajeria/src/Classes/M07_Template/StatusPackage/ApprovedStatus.java
@@ -2,6 +2,6 @@ package Classes.M07_Template.StatusPackage;
 
 public class ApprovedStatus implements IStatus{
     public String getStatusTemplate(){
-        return "Approved Template"
+        return "Approved Template";
     }
 }
