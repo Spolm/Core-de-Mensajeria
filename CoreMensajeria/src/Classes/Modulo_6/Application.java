68d8bc77d942691b59e48b26dfd743a87fa44206
Braulio <brauliopicon96@gmail.com>
2018-11-04 17:29:40 -0400

    Agregando modulo 6, origen de la data

---
+++ CoreMensajeria/src/Classes/Modulo_6/Application.java
@@ -0,0 +1,85 @@
+package Classes.Modulo_6;
+
+import Classes.User;
+
+import java.util.Date;
+
+public class Application {
+
+    private int _idApplication;
+    private String _nameApplication;
+    private String _descriptionApplication;
+    private String _tokenApplication;
+    private Date _dateOfCreateApplication;
+    private int _statusApplication;
+    private int _userCreatorApplication;
+
+    public Application(int _idApplication, String _nameApplication, String _descriptionApplication, String _tokenApplication, Date _dateOfCreateApplication, int _statusApplication, int _userCreatorApplication) {
+        this._idApplication = _idApplication;
+        this._nameApplication = _nameApplication;
+        this._descriptionApplication = _descriptionApplication;
+        this._tokenApplication = _tokenApplication;
+        this._dateOfCreateApplication = _dateOfCreateApplication;
+        this._statusApplication = _statusApplication;
+        this._userCreatorApplication = _userCreatorApplication;
+    }
+
+    public Application(){
+    }
+
+    public int get_idApplication() {
+        return _idApplication;
+    }
+
+    public void set_idApplication(int _idApplication) {
+        this._idApplication = _idApplication;
+    }
+
+    public String get_nameApplication() {
+        return _nameApplication;
+    }
+
+    public void set_nameApplication(String _nameApplication) {
+        this._nameApplication = _nameApplication;
+    }
+
+    public String get_descriptionApplication() {
+        return _descriptionApplication;
+    }
+
+    public void set_descriptionApplication(String _descriptionApplication) {
+        this._descriptionApplication = _descriptionApplication;
+    }
+
+    public String get_tokenApplication() {
+        return _tokenApplication;
+    }
+
+    public void set_tokenApplication(String _tokenApplication) {
+        this._tokenApplication = _tokenApplication;
+    }
+
+    public Date get_dateOfCreateApplication() {
+        return _dateOfCreateApplication;
+    }
+
+    public void set_dateOfCreateApplication(Date _dateOfCreateApplication) {
+        this._dateOfCreateApplication = _dateOfCreateApplication;
+    }
+
+    public int get_statusApplication() {
+        return _statusApplication;
+    }
+
+    public void set_statusApplication(int _statusApplication) {
+        this._statusApplication = _statusApplication;
+    }
+
+    public int get_userCreatorApplication() {
+        return _userCreatorApplication;
+    }
+
+    public void set_userCreatorApplication(int _userCreatorApplication) {
+        this._userCreatorApplication = _userCreatorApplication;
+    }
+}
--- CoreMensajeria/src/CoreMensajeria.java
+++ CoreMensajeria/src/CoreMensajeria.java
@@ -4,6 +4,7 @@ import webService.M01_Login.M01_Login;
 import webService.HelloWorld;
 import webService.M01_Login.M01_User;
 import webService.M02_CompanyManagement.M02_Companies;
+import webService.M06_DataOrigin.M06_Application;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
@@ -22,6 +23,7 @@ public class CoreMensajeria extends Application{
         h.add( M01_Login.class );
         h.add( M01_User.class );
         h.add(M02_Companies.class);
+        h.add( M06_Application.class );
         h.add(IntegratorServiceAPI.class);
         h.add(ChannelServiceAPI.class);
         return h;
