29b77ff76cbafd18368b8a0a552088efc01e631f
Brs_Tor <boristor22@gmail.com>
2018-11-06 18:14:07 -0400

    Update CREATES.sql

--- SCRIPTS/CREATES.sql
+++ SCRIPTS/CREATES.sql
@@ -127,11 +127,4 @@ create table public.Template_Status
     ts_template integer,
     ts_status integer,
     CONSTRAINT fk_template_id FOREIGN KEY ("ts_template") REFERENCES public.Template (tem_id),
-<<<<<<< HEAD
     CONSTRAINT fk_Status_id FOREIGN KEY ("ts_status") REFERENCES public.Status (sta_id)
-);
-
-=======
-    CONSTRAINT fk_Status_id FOREIGN KEY ("ts_id") REFERENCES public.Status (sta_id)
-);
->>>>>>> 1f621540d7841b10317870b767dc38606f619b65
