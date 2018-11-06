22d3fe00d5418961f6f24d11f06a154e4ceb67b2
Brs_Tor <boristor22@gmail.com>
2018-11-06 17:43:43 -0400

    Agregado template service que realiza las request http del frontend

---
+++ CoreMensajeriaFront/src/app/layout/template/template.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { map, catchError, tap } from 'rxjs/operators';
+
+const endpoint = 'http://localhost:8080/CoreMensajeria_war_exploded/';
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type':  'application/json'
+  })};
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class TemplateService {
+
+  constructor(private http: HttpClient) {
+    
+  }
+
+  private extractData(res: Response) {
+    let body = res;
+    return body || { };
+  }
+
+  getTemplates(){
+    return this.http.get(endpoint + 'templates').pipe(
+      map(this.extractData));
+  }
+
+}
