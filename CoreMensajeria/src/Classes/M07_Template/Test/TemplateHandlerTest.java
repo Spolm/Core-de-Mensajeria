dc5c5bb2f3e1361454424b34d58061d327bcac93
Derot <rubenlunamate@gmail.com>
2018-11-06 22:30:03 -0400

    Agregando metodos de prueba ObjectsNotNull y postTemplateStatusCorrectly en clase: TemplateHandlerTest.java

--- CoreMensajeria/src/Classes/M07_Template/Test/TemplateHandlerTest.java
+++ CoreMensajeria/src/Classes/M07_Template/Test/TemplateHandlerTest.java
@@ -4,6 +4,8 @@ import Classes.M07_Template.HandlerPackage.TemplateHandler;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.AfterEach;
@@ -18,25 +20,44 @@ public class TemplateHandlerTest {
     }
 
     @Test
-    void getTemplatesCorrectly(){
+    void ObjectsNotNull(){
         assertNotNull(templateHandler);
+        assertNotNull(templateHandler.getSql());
         assertNotNull(templateHandler.getTemplates());
-        assertEquals(templateHandler.getTemplates().size(), 5);
 
         for (int i=0 ; i < 5 ; i++){
             assertNotNull(templateHandler.getTemplates().get(i));
+        }
+
+        for (int i=1 ; i < 6 ; i++){
+            assertNotNull(templateHandler.getTemplate(i));
+        }
+    }
+
+    @Test
+    void getTemplatesCorrectly(){
+        assertEquals(templateHandler.getTemplates().size(), 5);
+
+        for (int i=0 ; i < 5 ; i++){
             assertEquals(templateHandler.getTemplates().get(i).getTemplateId(), i+1);
         }
     }
 
     @Test
     void getTemplateCorrectly(){
-        assertNotNull(templateHandler);
 
         for (int i=1 ; i < 6 ; i++){
-            assertNotNull(templateHandler.getTemplate(i));
             assertEquals(templateHandler.getTemplate(i).getTemplateId(), i);
         }
+    }
+
+    @Test
+    void postTemplateStatusCorrectly(){
+        assertTrue(templateHandler.postTemplateStatus(1));
+        assertFalse(templateHandler.postTemplateStatus(2));
+        assertTrue(templateHandler.postTemplateStatus(3));
+        assertFalse(templateHandler.postTemplateStatus(4));
+        assertFalse(templateHandler.postTemplateStatus(5));
     }
 
     @AfterEach
