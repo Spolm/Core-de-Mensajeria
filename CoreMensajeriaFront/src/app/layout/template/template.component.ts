dbc7aef70fd7f8d376cdee5a23ae342a0468c57c
Brs_Tor <boristor22@gmail.com>
2018-11-07 03:23:22 -0400

    Pop-up para confirmar el cambio de status

--- CoreMensajeriaFront/src/app/layout/template/template.component.ts
+++ CoreMensajeriaFront/src/app/layout/template/template.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { routerTransition } from '../../router.animations';
 import { TemplateService } from './template.service';
+import { ToastrService } from 'ngx-toastr';
+import { timeout } from 'q';
 
 @Component({
   selector: 'app-template',
@@ -14,7 +16,7 @@ export class TemplateComponent implements OnInit {
   templates: any = [];
   status = false;
 
-  constructor(private templateService: TemplateService) {
+  constructor(private templateService: TemplateService, private toastr: ToastrService) {
     templateService.getTemplates().subscribe(data => {
       this.templates = data;
     });
@@ -24,7 +26,13 @@ export class TemplateComponent implements OnInit {
   }
 
   approveTemplate(templateId: number){
-    console.log(templateId);
+    this.toastr.info("Para confirmar realice click de nuevo", "Aprobar la plantilla id: "+templateId,
+    {
+      timeOut: 5000,
+      progressBar: true,
+      positionClass: 'toast-top-left'
+    });
+    //HTTP POST
   }
 
 }
