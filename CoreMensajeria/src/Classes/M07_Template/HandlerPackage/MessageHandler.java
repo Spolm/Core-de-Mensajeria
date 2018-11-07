71b364e47adceca302eb118e9b1db1e4f5c9675f
Samuel Acevedo <acevedo.lamon.samuel@gmail.com>
2018-11-06 23:40:50 -0400

    Servicio de Message funcional 	- Respuesta de los mesajes con sus parametros correspondientes 	Nota: Aun no tiene asociado el templete correspondiente a dicho 	mensaje.

--- CoreMensajeria/src/Classes/M07_Template/HandlerPackage/MessageHandler.java
+++ CoreMensajeria/src/Classes/M07_Template/HandlerPackage/MessageHandler.java
@@ -27,16 +27,19 @@ public class MessageHandler {
                 message.setMessageId(resultSet.getInt("mes_id"));
                 message.setMessage(resultSet.getString("mes_text"));
                 ResultSet resultSetAux = sql.sqlConn(
-                        "SELECT PAR_ID,PAR_NAME FROM PARAMETER P" +
-                        "INNER JOIN MESSAGE_PARAMETER MP" +
-                        "ON P.PAR_ID = MP.MP_PARAMETER" +
+                        "SELECT PAR_ID,PAR_NAME FROM PARAMETER P " +
+                        "INNER JOIN MESSAGE_PARAMETER MP " +
+                        "ON P.PAR_ID = MP.MP_PARAMETER " +
                         "WHERE MP.MP_MESSAGE = " + message.getMessageId());
+                ArrayList<Parameter> parameterArrayList = new ArrayList<>();
                 while(resultSetAux.next()) {
                     Parameter parameter = new Parameter();
                     parameter.setParameterId(resultSetAux.getInt("par_id"));
                     parameter.setName(resultSetAux.getString("par_name"));
-                    message.addParameter(parameter);
+                    parameterArrayList.add(parameter);
                 }
+                message.setParameters(parameterArrayList);
+                messagesArrayList.add(message);
             }
         }catch (SQLException e) {
             e.printStackTrace();
--- CoreMensajeria/src/webService/M07_Template/M07_Message.java
+++ CoreMensajeria/src/webService/M07_Template/M07_Message.java
@@ -2,20 +2,16 @@ package webService.M07_Template;
 
 import Classes.M07_Template.HandlerPackage.MessageHandler;
 import Classes.M07_Template.MessagePackage.Message;
-import Classes.Sql;
 import com.google.gson.Gson;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
-import java.sql.Connection;
 import java.util.ArrayList;
 
 @Path("/")
 public class M07_Message {
 
     public Gson gson = new Gson();
-    private Connection conn = Sql.getConInstance();
 
     @GET
     public Response getMessages(){
