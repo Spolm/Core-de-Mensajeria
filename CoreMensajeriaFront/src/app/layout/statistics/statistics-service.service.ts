603ac549488460fc94e2e6563cbff5fd3d6042b9
Unknown <edwarddenobrega@gmail.com>
2018-11-19 19:55:52 -0400

    Mejorando metodos get de angular y arreglando la ruta para que funcione

--- CoreMensajeriaFront/src/app/layout/statistics/statistics-service.service.ts
+++ CoreMensajeriaFront/src/app/layout/statistics/statistics-service.service.ts
@@ -7,21 +7,28 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 })
 export class StatisticsServiceService {
 
+  private ApiURL : string = 'http://localhost:8080/CoreMensajeria_war_exploded/M09_Statistics/'
+
   constructor(private http: HttpClient) { }
+
+
+  //idea: lograr enviar los datos captados y en el back adaptar los metodos con los pathparam
+  // de modo tal que cuando apriete consultar ejecute 3 metodos( uno por cada grafica) que logre ajustarse
+  //a los parametros
 
 getStatisticsData1(){
 
-return this.http.get('http://localhost:8080/CoreMensajeria_war/M09_Statistics/MessageCompanyBar')
+return this.http.get(this.ApiURL + 'MessageCompanyBar')
 }
 
 getStatisticsData2(){
 
- return this.http.get('http://localhost:8080/CoreMensajeria_war/M09_Statistics/MessageCompanyLine') 
+ return this.http.get(this.ApiURL + 'MessageCompanyLine') 
 }
 
 getStatisticsData3(){
 
-  return this.http.get('http://localhost:8080/CoreMensajeria_war/M09_Statistics/MessageCompanyPie') 
+  return this.http.get(this.ApiURL + 'MessageCompanyPie') 
 }
 
 
