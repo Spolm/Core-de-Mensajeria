07b1477cae7d50e5dfafd78ae282a7d6d4a32a06
Brs_Tor <boristor22@gmail.com>
2018-11-20 01:10:09 -0400

    Refactorizando el template.component

--- CoreMensajeriaFront/src/app/layout/template/template.component.ts
+++ CoreMensajeriaFront/src/app/layout/template/template.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { routerTransition } from '../../router.animations';
 import { TemplateService } from './template.service';
-import { ToastrService } from 'ngx-toastr';
-import { timeout } from 'q';
 
 @Component({
   selector: 'app-template',
@@ -11,45 +9,21 @@ import { timeout } from 'q';
   animations: [routerTransition()]
 })
 
-export class TemplateComponent implements OnInit {
+export class TemplateComponent{
 
   templates: any = [];
-  status = false;
-  counter: number = 0;
-  lastTemplateId: number;
 
-  constructor(private templateService: TemplateService, private toastr: ToastrService) {
-    templateService.getTemplates().subscribe(data => {
-      this.templates = data;
-    });
+  constructor(private templateService: TemplateService) {
+    this.getTemplates();
   }
 
-  ngOnInit() {
+  getTemplates(){
     this.templateService.getTemplates().subscribe(data => {
       this.templates = data;
     });
-    console.log("Cargue de nuevo");
   }
 
-  approveTemplate(templateId: number){
-    this.toastr.info("Para confirmar realice doble click de nuevo", "Aprobar la plantilla id: "+templateId,
-    {
-      timeOut: 2800,
-      progressBar: true
-    });
-    this.counter++;
-    if(this.counter == 2 && this.lastTemplateId == templateId){
-      this.templateService.approveTemplate(templateId);
-      this.toastr.success("Aprobada", "Plantilla id: "+templateId,
-      {
-        timeOut: 2800,
-        progressBar: true
-      });
-      this.counter = 0;
-      this.ngOnInit();
-    }
-    if(this.counter >= 2) this.counter = 0;
-    this.lastTemplateId = templateId;
+  approveTemplates(){
+    this.getTemplates();
   }
-
 }
