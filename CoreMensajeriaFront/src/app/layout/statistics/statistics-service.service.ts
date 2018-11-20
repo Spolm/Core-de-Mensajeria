26380275e52d46e87ef1286bb816fad3cfb3b7e0
Ignacio Paradisi <ignacioparadisi@gmail.com>
2018-11-20 17:35:43 -0400

    Se agregaron unos punto y comas faltantes

--- CoreMensajeriaFront/src/app/layout/statistics/statistics-service.service.ts
+++ CoreMensajeriaFront/src/app/layout/statistics/statistics-service.service.ts
@@ -7,7 +7,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 })
 export class StatisticsServiceService {
 
-  private ApiURL : string = 'http://localhost:8080/CoreMensajeria_war_exploded/M09_Statistics/'
+  private ApiURL : string = 'http://localhost:8080/CoreMensajeria_war_exploded/M09_Statistics/';
 
   constructor(private http: HttpClient) { }
 
@@ -16,20 +16,16 @@ export class StatisticsServiceService {
   // de modo tal que cuando apriete consultar ejecute 3 metodos( uno por cada grafica) que logre ajustarse
   //a los parametros
 
-getStatisticsData1(){
+    getStatisticsData1() {
+        return this.http.get(this.ApiURL + 'MessageCompanyBar');
+    }
 
-return this.http.get(this.ApiURL + 'MessageCompanyBar')
-}
+    getStatisticsData2() {
+          return this.http.get(this.ApiURL + 'MessageCompanyLine');
+    }
 
-getStatisticsData2(){
-
- return this.http.get(this.ApiURL + 'MessageCompanyLine') 
-}
-
-getStatisticsData3(){
-
-  return this.http.get(this.ApiURL + 'MessageCompanyPie') 
-}
-
+    getStatisticsData3() {
+          return this.http.get(this.ApiURL + 'MessageCompanyPie');
+    }
 
 }
