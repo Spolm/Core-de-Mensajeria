7a435b0bed82a0c1e64e6448aa750266936700ea
kamgmc <kamgmc@gmail.com>
2018-11-23 17:48:07 -0400

    Actualizacion Rest Service
    Se actualizo el endpoint.
    Se limito el manejo de errores.

--- CoreMensajeriaFront/src/app/shared/services/rest.service.ts
+++ CoreMensajeriaFront/src/app/shared/services/rest.service.ts
@@ -8,7 +8,7 @@ import { map, catchError, tap } from 'rxjs/operators';
 })
 export class RestService {
 
-  endpoint = 'http://localhost:8080/CoreMensajeria_war/';
+  endpoint = 'http://localhost:8080/CoreMensajeria_war_exploded/';
   httpOptions = {
     headers: new HttpHeaders({
       'Content-Type':  'application/json'
@@ -33,37 +33,31 @@ export class RestService {
   }
   
   addData (component, data): Observable<any> {
-    console.log(data);
     return this.http.post<any>(this.endpoint + component, JSON.stringify(data), this.httpOptions).pipe(
-      catchError(this.handleError<any>('addProduct'))
+      //catchError(this.handleError<any>('addProduct'))
     );
   }
   
   updateData (component, data, id): Observable<any> {
     return this.http.put(this.endpoint + component + '/' + id, JSON.stringify(data), this.httpOptions).pipe(
-      tap(_ => console.log(`updated id=${id}`)),
-      catchError(this.handleError<any>('updateData'))
+      tap(_ => console.log(`updated id=${id}`))/*,
+      catchError(this.handleError<any>('updateData'))*/
     );
   }
   
   deleteData (component, id): Observable<any> {
     return this.http.delete<any>(this.endpoint + component + '/' + id, this.httpOptions).pipe(
-      tap(_ => console.log(`deleted id=${id}`)),
-      catchError(this.handleError<any>('deleteData'))
+      tap(_ => console.log(`deleted id=${id}`))/*,
+      catchError(this.handleError<any>('deleteData'))*/
     );
   }
-  
+  /*
   private handleError<T> (operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
   
-      // TODO: send the error to remote logging infrastructure
-      console.error(error); // log to console instead
-  
-      // TODO: better job of transforming error for user consumption
+      console.error(error);
       console.log(`${operation} failed: ${error.message}`);
-  
-      // Let the app keep running by returning an empty result.
       return of(result as T);
     };
-  }
+  }*/
 }
