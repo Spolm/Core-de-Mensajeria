5998b277e5db2c7b0b130ed9187068d91a426609
fercon997 <fercon997@gmail.com>
2018-11-26 18:50:28 -0400

    Se colocó IF EXISTS a todas las lineas de alter sequence de modo que no hayan errores

--- SCRIPTS/DROPS.sql
+++ SCRIPTS/DROPS.sql
@@ -1,19 +1,19 @@
 \c CoreMensajeria
-ALTER SEQUENCE application_app_id_seq RESTART;
-ALTER SEQUENCE campaign_cam_id_seq RESTART;
-ALTER SEQUENCE channel_cha_id_seq RESTART;
-ALTER SEQUENCE channel_integrator_ci_id_seq RESTART;
-ALTER SEQUENCE company_com_id_seq RESTART;
-ALTER SEQUENCE integrator_int_id_seq RESTART;
-ALTER SEQUENCE message_mes_id_seq RESTART;
-ALTER SEQUENCE message_parameter_mp_id_seq RESTART;
-ALTER SEQUENCE parameter_par_id_seq RESTART;
-ALTER SEQUENCE sent_message_sen_id_seq RESTART;
-ALTER SEQUENCE status_sta_id_seq RESTART;
-ALTER SEQUENCE template_channel_integrator_tci_id_seq RESTART;
-ALTER SEQUENCE template_status_ts_id_seq RESTART;
-ALTER SEQUENCE template_tem_id_seq RESTART;
-ALTER SEQUENCE user_use_id_seq RESTART;
-\C postgres
+ALTER SEQUENCE IF EXISTS application_app_id_seq RESTART;
+ALTER SEQUENCE IF EXISTS campaign_cam_id_seq RESTART;
+ALTER SEQUENCE IF EXISTS channel_cha_id_seq RESTART;
+ALTER SEQUENCE IF EXISTS channel_integrator_ci_id_seq RESTART;
+ALTER SEQUENCE IF EXISTS company_com_id_seq RESTART;
+ALTER SEQUENCE IF EXISTS integrator_int_id_seq RESTART;
+ALTER SEQUENCE IF EXISTS message_mes_id_seq RESTART;
+ALTER SEQUENCE IF EXISTS message_parameter_mp_id_seq RESTART;
+ALTER SEQUENCE IF EXISTS parameter_par_id_seq RESTART;
+ALTER SEQUENCE IF EXISTS sent_message_sen_id_seq RESTART;
+ALTER SEQUENCE IF EXISTS status_sta_id_seq RESTART;
+ALTER SEQUENCE IF EXISTS template_channel_integrator_tci_id_seq RESTART;
+ALTER SEQUENCE IF EXISTS template_status_ts_id_seq RESTART;
+ALTER SEQUENCE IF EXISTS template_tem_id_seq RESTART;
+ALTER SEQUENCE IF EXISTS user_use_id_seq RESTART;
+\c postgres
 DROP DATABASE IF EXISTS "CoreMensajeria_StarSchema";
 DROP DATABASE IF EXISTS "CoreMensajeria";
