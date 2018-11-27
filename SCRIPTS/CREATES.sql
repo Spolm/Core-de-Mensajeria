cc0020d9605c316ab14c4225af894354dee100ef
fabian graterol <fabiancgraterol@gmail.com>
2018-11-26 21:03:43 -0400

    solucionando conflictos

--- SCRIPTS/CREATES.sql
+++ SCRIPTS/CREATES.sql
@@ -169,3 +169,8 @@ ALTER TABLE Template
 ADD COLUMN tem_application_id integer NOT NULL,
 ADD CONSTRAINT fk_application_id FOREIGN KEY ("tem_application_id")
 REFERENCES Application(app_id);
+
+ALTER TABLE Parameter
+ADD COLUMN par_company_id integer,
+ADD CONSTRAINT fk_par_company_id FOREIGN KEY ("par_company_id") 
+REFERENCES Company (com_id);
