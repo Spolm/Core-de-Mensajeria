f0d38c11d78e7d68a06378eda323baa575d4c095
rigojr <rigojunnior@gmail.com>
2018-12-01 11:25:54 -0400

    limpiesa del código en integradores

--- CoreMensajeriaFront/src/app/layout/integrator/api.service.ts
+++ CoreMensajeriaFront/src/app/layout/integrator/api.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
+import { Http } from '@angular/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { catchError } from 'rxjs/operators';
