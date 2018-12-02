c38b7133c9db72bd97e70498e07b03c5ab298c62
marcolbx <marcolbx@hotmail.com>
2018-12-01 20:32:49 -0400

    Agregado clase MailSender
    Es capaz de enviar emails

---
+++ CoreMensajeria/Common/src/main/java/Classes/M01_Login/MailSender.java
@@ -0,0 +1,50 @@
+package Classes.M01_Login;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+public class MailSender {
+
+    static Properties mailServerProperties;
+    static Session getMailSession;
+    static MimeMessage generateMailMessage;
+
+    public static void generateAndSendEmail(String link,String email) throws AddressException, MessagingException {
+
+
+        // Step1
+        System.out.println("\n 1st ===> setup Mail Server Properties..");
+        mailServerProperties = System.getProperties();
+        mailServerProperties.put("mail.smtp.port", "587");
+        mailServerProperties.put("mail.smtp.auth", "true");
+        mailServerProperties.put("mail.smtp.starttls.enable", "true");
+        System.out.println("Mail Server Properties have been setup successfully..");
+
+        // Step2
+        System.out.println("\n\n 2nd ===> get Mail Session..");
+        getMailSession = Session.getDefaultInstance(mailServerProperties, null);
+        generateMailMessage = new MimeMessage(getMailSession);
+        generateMailMessage.addRecipient(Message.RecipientType.TO, new InternetAddress(email));
+        generateMailMessage.setSubject("Recuperación de contraseña");
+        String emailBody = " Ingresa en el siguiente link para recuperar tu contraseña: " + link + "<br><br> Saludos cordiales, <br>CoreMensajeria Support";
+        generateMailMessage.setContent(emailBody, "text/html");
+        System.out.println("Mail Session has been created successfully..");
+
+        // Step3
+        System.out.println("\n\n 3rd ===> Get Session and Send mail");
+        Transport transport = getMailSession.getTransport("smtp");
+
+        // Enter your correct gmail UserID and Password
+        // if you have 2FA enabled then provide App Specific Password
+        transport.connect("smtp.gmail.com", "coremensajeria2018", "coremensajeria1234");
+        transport.sendMessage(generateMailMessage, generateMailMessage.getAllRecipients());
+        transport.close();
+    }
+}
+
