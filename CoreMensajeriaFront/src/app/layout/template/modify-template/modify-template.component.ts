eccb03b51f75a422a8cc3446a809d6e44d2cc26b
Brs_Tor <boristor22@gmail.com>
2018-12-02 16:12:32 -0400

    Agregando  modify-template component

---
+++ CoreMensajeriaFront/src/app/layout/template/modify-template/modify-template.component.ts
@@ -0,0 +1,145 @@
+import { Component } from '@angular/core';
+import { TemplateService } from '../template.service';
+import { ToastrService } from 'ngx-toastr';
+import { ActivatedRoute } from '@angular/router';
+import { delay } from 'q';
+
+@Component({
+  selector: 'app-modify-template',
+  templateUrl: './modify-template.component.html',
+  styleUrls: ['./modify-template.component.scss']
+})
+export class ModifyTemplateComponent {
+
+  templateId: any;
+  sub: any;
+  templateJson: any = [];
+  parametersJson: any = [];
+  channelsJson: any = [];
+  formMessage: string;
+  parameters: Array<string> = [];
+  newParameters: Array<string> = [];
+  channels_integrators: any = [];
+  showInputCreateParameterState = false;
+
+  constructor(private templateService: TemplateService, private toastr: ToastrService, private route: ActivatedRoute) {
+    this.sub = this.route.params.subscribe(params => {
+      this.templateId = +params['id'];
+    });
+    this.getParameters();
+    this.getChannels();
+    this.getTemplate();
+  }
+
+  getParameters() {
+    this.templateService.getParameters(1).subscribe(data => {
+      this.parametersJson = data;
+    });
+  }
+
+  getChannels() {
+    this.templateService.getChannels().subscribe(data => {
+      this.channelsJson = data;
+    });
+  }
+
+  async getTemplate() {
+    this.templateService.getTemplate(this.templateId).subscribe(data => {
+      this.templateJson = data;
+    });
+    await delay(1000);
+    this.formMessage = this.templateJson.message.message;
+    this.assignParameter(this.parameters , this.templateJson.message.parameterArrayList);
+    this.assignChannelsIntegrators(this.channels_integrators, this.templateJson.channels);
+  }
+
+  assignParameter(place: Array<any>, data: Array<any>){
+    data.forEach( (value) => {
+      place.push(value.name);
+    })
+  }
+
+  assignChannelsIntegrators(place: Array<any>, data: Array<any>){
+    data.forEach( (channel) => {      
+      channel.integrators.forEach( (integrator) =>{
+        place.push(
+          { channel, integrator}
+          );
+      })
+    })
+  }
+
+  addParameter(message: string, parameterName: string) {
+    if (!this.parameters.find(x => x == parameterName)) {
+      const myFormMessage = document.getElementById('formMessage');
+      const pointer = (myFormMessage as HTMLTextAreaElement).selectionStart;
+      const startMessage = message.slice(0, pointer);
+      const endMessage = message.slice(pointer, message.length);
+      this.parameters.push(parameterName);
+      this.formMessage = startMessage + ' [.$' + parameterName + '$.] ' + endMessage;
+    }
+  }
+
+  addNewParameter(message: string, parameterName: string) {
+    this.addParameter(message, parameterName);
+    if (!this.newParameters.find(x => x == parameterName)) {
+      this.newParameters.push(parameterName);
+    }
+    this.showInputCreateParameterState = false;
+  }
+
+  addIntegrator(channel: any, integratorId: number) {
+    if (!this.channels_integrators.find(x => x.channel.idChannel == channel.idChannel)) {
+      if (!this.channels_integrators.find(x => x.integrator.idIntegrator == integratorId)) {
+        const integrator = channel.integrators.find(x => x.idIntegrator == integratorId);
+        this.channels_integrators.push(
+          { channel, integrator }
+        );
+      }
+    }
+  }
+
+  showInputCreateParameter() {
+    this.showInputCreateParameterState = true;
+  }
+
+  deleteParameter(message: string, parameterName: string) {
+    const pointer = message.search(parameterName) - 4;
+    const startMessage = message.slice(0, pointer);
+    const endMessage = message.slice(pointer + parameterName.length + 8, message.length);
+    this.formMessage = startMessage + endMessage;
+    this.parameters.splice(this.parameters.indexOf(parameterName), 1);
+    if (this.newParameters.find(x => x == parameterName)) {
+      this.newParameters.splice(this.newParameters.indexOf(parameterName), 1);
+    }
+  }
+
+  deleteChannel_Integrator(channel_integrator) {
+    this.channels_integrators.splice(this.channels_integrators.indexOf(channel_integrator), 1);
+  }
+
+  postTemplate() {
+
+    console.log(this.formMessage);
+    console.log(this.parameters);
+    console.log(this.newParameters);
+    console.log(this.channels_integrators);
+    if ((this.formMessage !== undefined) && (this.formMessage.length > 5)) {
+      if (this.channels_integrators[0]) {
+        this.templateService.postTemplate(this.formMessage, this.parameters, this.newParameters, 1, this.channels_integrators);
+      } else {
+        this.toastr.error('Falta llenar un campo', 'Error',
+          {
+            timeOut: 2800,
+            progressBar: true
+          });
+      }
+    } else {
+      this.toastr.error('Tal vez quiera escribir un mensaje mas largo', 'Error',
+        {
+          timeOut: 2800,
+          progressBar: true
+        });
+    }
+  }
+}
--- CoreMensajeriaFront/src/app/layout/template/template-routing.module.ts
+++ CoreMensajeriaFront/src/app/layout/template/template-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { TemplateComponent } from './template.component';
 import { CreateTemplateComponent } from './create-template/create-template.component';
 import { TemplatePageComponent } from './template-page/template-page.component';
+import { ModifyTemplateComponent } from './modify-template/modify-template.component';
 
 const routes: Routes = [
   { path: '', component: TemplateComponent },
   { path: 'create', component: CreateTemplateComponent },
-  { path: ':id', component: TemplatePageComponent}
+  { path: ':id', component: TemplatePageComponent },
+  { path: 'modify/:id', component: ModifyTemplateComponent }
 ];
 
 @NgModule({
