01ec84a334de5b7728319b90af99d7224f3874c7
Spolm <luismachadoz.12@gmail.com>
2018-12-03 14:25:19 -0400

    Documentacion y excepcion nueva

--- CoreMensajeria/Common/src/main/java/Classes/M02_Company/CompanyDAO.java
+++ CoreMensajeria/Common/src/main/java/Classes/M02_Company/CompanyDAO.java
@@ -2,6 +2,8 @@ package Classes.M02_Company;
 
 import Classes.Sql;
 import Exceptions.CompanyDoesntExistsException;
+import Exceptions.ParameterCantBeNullException;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -115,6 +117,24 @@ public class CompanyDAO {
             e.printStackTrace();
         }
         return co.get_status();
+    }
+
+    public Company createCompany (Company co) throws CompanyDoesntExistsException, ParameterCantBeNullException {
+        try {
+
+            PreparedStatement preparedStatement = conn.prepareCall("{Call m02_addcompany}");
+            preparedStatement.setInt(1, co.get_idCompany());
+            preparedStatement.setString(2, co.get_name());
+            preparedStatement.setString(3, co.get_desc());
+            preparedStatement.setBoolean(4, co.get_status());
+            preparedStatement.setString(5, co.get_link());
+            preparedStatement.execute();
+        }catch (SQLException e){
+            throw new CompanyDoesntExistsException(e);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return co;
     }
 
 }
