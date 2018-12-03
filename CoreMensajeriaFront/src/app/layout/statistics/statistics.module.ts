b78e5938a15f32768dbf5065942923f6c85142e4
Sagg_301 <sagg_301@outlook.com>
2018-12-02 22:25:55 -0400

    companias en el front

--- CoreMensajeriaFront/src/app/layout/statistics/statistics.module.ts
+++ CoreMensajeriaFront/src/app/layout/statistics/statistics.module.ts
@@ -6,15 +6,15 @@ import { CommonModule } from "@angular/common";
 import { StatisticsComponent } from "./statistics.component";
 import { StatisticsRoutingModule } from "./statistics-routing.module";
 import { PlotlyModule } from "angular-plotly.js";
-//import { NgMultiSelectDropDownModule } from "ng-multiselect-dropdown";
+import { NgMultiSelectDropDownModule } from "ng-multiselect-dropdown";
 
 @NgModule({
     imports: [
         CommonModule,
         StatisticsRoutingModule,
         PlotlyModule,
-        FormsModule
-        //NgMultiSelectDropDownModule.forRoot()
+        FormsModule,
+        NgMultiSelectDropDownModule.forRoot()
     ],
     providers: [StatisticsServiceService],
     declarations: [StatisticsComponent]
