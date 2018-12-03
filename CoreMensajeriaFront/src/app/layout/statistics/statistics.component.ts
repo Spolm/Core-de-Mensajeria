fca812c4469d1e719a7b81be7007542915454082
Ignacio Paradisi <ignacioparadisi@gmail.com>
2018-12-03 19:58:44 -0400

    Se eliminaron métodos que no se utilizan

--- CoreMensajeriaFront/src/app/layout/statistics/statistics.component.ts
+++ CoreMensajeriaFront/src/app/layout/statistics/statistics.component.ts
@@ -291,51 +291,6 @@ export class StatisticsComponent extends DropdownMethods implements OnInit {
         });
     }
 
-    // capturar() {
-    //     if (this.opcionSeleccionado != "0") {
-    //         // Pasamos el valor seleccionado a la variable verSeleccion
-    //         this.verSeleccion = this.opcionSeleccionado;
-    //         console.log("Valor Capturado", this.verSeleccion);
-    //     } else this.toastr.error("Debe seleccionar otra opcion");
-    // }
-
-    capturarDate() {
-        if (
-            this.opcionDateSleccionado != null &&
-            this.opcionDateSleccionado2 != null &&
-            this.opcionDateSleccionado < this.opcionDateSleccionado2
-        ) {
-            this.Date1Capturado =
-                "?paramDate1=" + this.opcionDateSleccionado.toString();
-            this.Date2Capturado =
-                "?paramDate2=" + this.opcionDateSleccionado2.toString();
-            this.paramType = "paramType=" + this.verSeleccion;
-            console.log(
-                "FechaCapturada",
-                "Dates1: " + this.Date1Capturado,
-                "Dates2: " + this.Date2Capturado,
-                +" " + new Date(this.opcionDateSleccionado).getUTCDate(),
-                new Date(this.opcionDateSleccionado).getUTCMonth(),
-                new Date(this.opcionDateSleccionado).getFullYear(),
-                new Date(this.opcionDateSleccionado2).getUTCDate(),
-                new Date(this.opcionDateSleccionado2).getUTCMonth(),
-                new Date(this.opcionDateSleccionado2).getFullYear()
-            );
-        } else if (
-            (this.opcionDateSleccionado == null &&
-                this.opcionDateSleccionado2 == null) ||
-            (this.opcionDateSleccionado.toString().length == 0 &&
-                this.opcionDateSleccionado2.toString().length == 0)
-        ) {
-            console.log("hols" + this.opcionDateSleccionado.toString().length);
-
-            this.Date1Capturado = "?paramDate1=";
-            this.Date2Capturado = "?paramDate2=";
-            this.paramType = "paramType=" + this.verSeleccion;
-            this.toastr.error("Error en las fechas");
-        }
-    }
-
     // Handle company selecction
     companySelected(company: any) {
         this.selectedCompaniesIds.push(company["company_id"]);
