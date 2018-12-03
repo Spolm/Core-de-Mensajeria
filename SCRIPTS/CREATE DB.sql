a22137507ed7c2cc0483c3b8e3b358af8fafbacf
gabrielbaron16 <gabrielbu161@gmail.com>
2018-12-03 14:31:52 -0400

    Arreglando Prueba Unitaria y Comentando métodos

--- SCRIPTS/CREATE DB.sql
+++ SCRIPTS/CREATE DB.sql
@@ -8,8 +8,8 @@ CREATE DATABASE "CoreMensajeria"
     ENCODING = 'UTF8'
     --LC_COLLATE = 'English_United States.1252'
     --LC_CTYPE = 'English_United States.1252'
-    --LC_COLLATE = 'C'
-    --LC_CTYPE = 'UTF-8'
+    LC_COLLATE = 'C'
+    LC_CTYPE = 'UTF-8'
     TABLESPACE = pg_default
     CONNECTION LIMIT = -1;
 
