9b4e1aec1960e252b2e4f55b859d143731b1c138
rafael sanchez <rafaelsanchez814@gmail.com>
2018-12-26 14:22:50 -0400

    DtoFactory
    add factory and campaign in dto factory

--- CoreMensajeria/Services/src/main/java/Factory/DTOFactory.java
+++ CoreMensajeria/Services/src/main/java/Factory/DTOFactory.java
@@ -1,9 +1,24 @@
 package Factory;
 import DTO.M02_DTO.DTOCompany;
+import DTO.M03_DTO.DTOCampaign;
+
+ /**
+ * Fabrica que instancia todos los dto
+ */
 public class DTOFactory {
 
-
+      /**
+      * Crea objeto Company
+      */
     public static DTOCompany CreateDtoCompany(){
         return new DTOCompany();
     }
+      /**
+      * Crea objeto Campaign
+      */
+    public static DTOCampaign CreateDtoCampaign() {
+        return new DTOCampaign();
+    }
+
+
 }
