1905cb684b355f7d0c6947d0ab09994ba81bdaf9
Unknown <edwarddenobrega@gmail.com>
2018-12-26 17:52:54 -0400

    Agregando a DTOFactory

--- CoreMensajeria/Services/src/main/java/Factory/DTOFactory.java
+++ CoreMensajeria/Services/src/main/java/Factory/DTOFactory.java
@@ -1,24 +1,64 @@
 package Factory;
 import DTO.M02_DTO.DTOCompany;
 import DTO.M03_DTO.DTOCampaign;
+import Entities.M02_Company.Company;
 
- /**
+import java.util.Date;
+
+/**
  * Fabrica que instancia todos los dto
  */
 public class DTOFactory {
 
       /**
-      * Crea objeto Company
-      */
-    public static DTOCompany CreateDtoCompany(){
-        return new DTOCompany();
+       * Metodos que instancian un obejto del tipo DTOCompany mediante los parametros pasados
+       * @return un objeto del tipo DTOCompany
+       */
+    public static DTOCompany CreateDtoCompanyWithOut_idAndlink(String name, String desc, boolean status){
+
+        return new DTOCompany(name, desc, status);
     }
-      /**
-      * Crea objeto Campaign
-      */
-    public static DTOCampaign CreateDtoCampaign() {
-        return new DTOCampaign();
+
+    public static DTOCompany CreateDtoCompanyWithOut_Link(int id, String name, String desc, boolean status){
+
+        return new DTOCompany(id, name, desc, status);
     }
+
+     public static DTOCompany CreateDtoFullCompany(int id, String name, String desc, boolean status, String link){
+
+         return new DTOCompany(id, name, desc, status, link);
+     }
+
+
+     /**
+      * Metodos que instancian un obejto del tipo DTOCampaign mediante los parametros pasados
+      * @return un objeto del tipo DTOCampaign
+      */
+    public static DTOCampaign CreateDtoCampaignWithOut_id_And_Company(String nameCampaign, String descCampaign,
+                                                                      boolean statusCampaign, Date startCampaign,
+                                                                      Date endCampaign) {
+
+        return new DTOCampaign(nameCampaign, descCampaign, statusCampaign, startCampaign, endCampaign);
+    }
+
+    public static DTOCampaign CreateDtoCampaignWithOut_Company(int idCampaign, String nameCampaign, String descCampaign,
+                                                               boolean statusCampaign, Date startCampaign,
+                                                               Date endCampaign) {
+
+        return new DTOCampaign(idCampaign, nameCampaign, descCampaign, statusCampaign, startCampaign, endCampaign);
+    }
+
+    public static DTOCampaign CreateDtoCampaignWithOut_Company(int idCampaign, String nameCampaign, String descCampaign,
+                                                               boolean statusCampaign, Date startCampaign,
+                                                               Date endCampaign, Company company) {
+
+        return new DTOCampaign(idCampaign, nameCampaign, descCampaign, statusCampaign, startCampaign,
+                               endCampaign, company);
+    }
+
+
+
+
 
 
 }
