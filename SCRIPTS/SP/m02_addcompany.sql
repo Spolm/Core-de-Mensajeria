5458d7633306aeea3c8eb0f94fa78986996c9deb
rafael sanchez <rafaelsanchez814@gmail.com>
2018-12-30 18:40:06 -0400

    sp modified with user id
    se modifico el sp para aceptar parametro que se declaro como OBLIGATORIO en tabla company

--- SCRIPTS/SP/m02_addcompany.sql
+++ SCRIPTS/SP/m02_addcompany.sql
@@ -1,16 +1,31 @@
 -- Function: m02_addcompany(character varying, character varying, boolean, character varying)
 
---DROP FUNCTION m02_addcompany(character varying, character varying, boolean, character varying);
+/*DROP FUNCTION m02_addcompany ( _name character varying, 
+										   _description character varying,
+										   _status boolean, 
+										   _link character varying,
+										    _userId int
+										  );*/
 
-CREATE OR REPLACE FUNCTION m02_addcompany ( _name character varying, _description character varying, _status boolean, _link character varying)
+CREATE OR REPLACE FUNCTION m02_addcompany ( _name character varying, 
+										   _description character varying,
+										   _status boolean, 
+										   _link character varying,
+										    _userId int
+										  )
     RETURNS VOID AS
 $BODY$
 BEGIN
-INSERT INTO public.company (com_name, com_description, com_status, com_route_link)
-VALUES (_name, _description, _status, _link);
+INSERT INTO public.company (com_name, com_description, com_status, com_route_link,com_user_id)
+VALUES (_name, _description, _status, _link,_userId);
 END;
 $BODY$
     LANGUAGE plpgsql VOLATILE
     COST 100;
-ALTER FUNCTION m02_addcompany (character varying, character varying, boolean, character varying)
+ALTER FUNCTION m02_addcompany ( _name character varying, 
+										   _description character varying,
+										   _status boolean, 
+										   _link character varying,
+										    _userId int
+										  )
     OWNER TO "CoreMensajeria";
