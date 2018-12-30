cb10bb96fa2c4b9ada18d846dd5220c01db04e1a
Ignacio Paradisi <ignacioparadisi@gmail.com>
2018-12-30 14:17:43 -0400

    Se agregó la instancia del comando SendMessage

--- CoreMensajeria/LogicLayer/src/main/java/Logic/CommandsFactory.java
+++ CoreMensajeria/LogicLayer/src/main/java/Logic/CommandsFactory.java
@@ -2,10 +2,13 @@ package Logic;
 
 import Entities.Entity;
 import Logic.M01_Login.GetUser;
+import Logic.M08_SendMessage.SendMessage;
 
 public class CommandsFactory {
 
     public static Command instanciateGetUser ( Entity user) {
         return new GetUser(user);
     }
+
+    public static Command createSendMessage() { return new SendMessage(); }
 }
