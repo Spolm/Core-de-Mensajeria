86bee0cd3b1286dd3b44b6ac3f997522e737c247
gabrielbaron16 <gabrielbu161@gmail.com>
2019-01-01 23:37:59 -0400

    Añadiendo Comando Compuesto para Validar Parámetros

---
+++ CoreMensajeria/Common/src/main/java/Entities/M08_Validation/XMLManagement/CommandValidateParameter.java
@@ -0,0 +1,24 @@
+package Entities.M08_Validation.XMLManagement;
+
+public abstract class CommandValidateParameter extends Command{
+    private String _response;
+    private boolean _valid;
+
+    public abstract void execute();
+
+    public String get_response() {
+        return _response;
+    }
+
+    public boolean is_valid() {
+        return _valid;
+    }
+
+    public void set_response(String _response) {
+        this._response = _response;
+    }
+
+    public void set_valid(boolean _valid) {
+        this._valid = _valid;
+    }
+}
