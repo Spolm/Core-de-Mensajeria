0aaa7c9f7f3d4e2a50f0fd83a7f2286a792e9fd4
gabrielbaron16 <gabrielbu161@gmail.com>
2019-01-01 23:39:13 -0400

    Comando para Validar Plantilla

---
+++ CoreMensajeria/Common/src/main/java/Entities/M08_Validation/XMLManagement/CommandValidateTemplate.java
@@ -0,0 +1,29 @@
+package Entities.M08_Validation.XMLManagement;
+
+import Entities.M07_Template.HandlerPackage.TemplateHandler;
+import Entities.M07_Template.Template;
+import Exceptions.TemplateDoesntExistsException;
+
+import java.util.logging.Logger;
+
+public class CommandValidateTemplate extends CommandValidateParameter {
+    int _id;
+
+    public CommandValidateTemplate(int _id) {
+        this._id = _id;
+    }
+
+    public void execute(){
+        Logger logger = Logger.getLogger(CommandValidateParameter.class.getName());
+        TemplateHandler template = new TemplateHandler();
+        try{
+            Template t=template.getTemplate(this._id);
+            if (t.getTemplateId()>0)
+                this.set_valid(true);
+        } catch (TemplateDoesntExistsException e) {
+            logger.warning("Plantilla no Existe");
+            this.set_valid(false);
+            this.set_response("Plantilla no Existe");
+        }
+    }
+}
