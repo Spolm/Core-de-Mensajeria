496089688191d53b98be6251daa0809fdaa02eb9
rafael sanchez <rafaelsanchez814@gmail.com>
2019-01-03 18:19:16 -0400

    spAddCompany

--- SCRIPTS/SP/m02_addcompany.sql
+++ SCRIPTS/SP/m02_addcompany.sql
@@ -17,14 +17,14 @@ CREATE OR REPLACE FUNCTION m02_addcompany ( _name character varying,
 $BODY$
 BEGIN
 INSERT INTO public.company (com_name, com_description, com_status, com_route_link,com_user_id)
-VALUES (_name, _description, _status, _link,_userId);
+VALUES (_name, _description, true, _link,_userId);
 END;
 $BODY$
     LANGUAGE plpgsql VOLATILE
     COST 100;
 ALTER FUNCTION m02_addcompany ( _name character varying, 
 										   _description character varying,
-										   true, 
+										   _status boolean, 
 										   _link character varying,
 										    _userId int
 										  )
