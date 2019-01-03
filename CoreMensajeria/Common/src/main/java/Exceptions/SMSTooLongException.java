4870c3208d0e7e5d8dd84b23f1a0c519dd9edec5
Fernando Consalvo <fconsalvo97@gmail.com>
2019-01-03 18:31:19 -0400

    Documentación de los métodos

--- CoreMensajeria/Common/src/main/java/Exceptions/SMSTooLongException.java
+++ CoreMensajeria/Common/src/main/java/Exceptions/SMSTooLongException.java
@@ -1,16 +1,28 @@
 package Exceptions;
 
+/**
+ * Excepción personalizada para manejar el tamaño del sms.
+ */
 public class SMSTooLongException extends Exception {
     public final int ERROR_CODE = 562;
     public final String  ERROR_MSG = "Ha ocurrido un errror "+ERROR_CODE+" SMS supera 160 caracteres";
 
+    /**
+     * Constructor vacío.
+     */
     public SMSTooLongException(){}
 
+    /**
+     * @param e recibe la excepción que se esta encapsulando.
+     */
     public SMSTooLongException(Exception e) {
         super(e);
 
     }
 
+    /**
+     * @return un string con el código y mensaje de error junto con el error original.
+     */
     @Override
     public String toString(){
         StringBuilder str = null;
