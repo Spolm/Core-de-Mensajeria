a6bd50f31d8e806d3dec86010861566783ba5b7a
Fernando Consalvo <fconsalvo97@gmail.com>
2019-01-05 11:34:10 -0400

    Se creo el DAO de SentMessage

---
+++ CoreMensajeria/Persistence/src/main/java/Persistence/M08_SendMessage/DAOSentMessage.java
@@ -0,0 +1,20 @@
+package Persistence.M08_SendMessage;
+
+import Entities.Entity;
+
+public class DAOSentMessage implements IDAOSentMessage{
+    @Override
+    public void create(Entity e) {
+
+    }
+
+    @Override
+    public Entity read(Entity e) {
+        return null;
+    }
+
+    @Override
+    public Entity update(Entity e) {
+        return null;
+    }
+}
---
+++ CoreMensajeria/Persistence/src/main/java/Persistence/M08_SendMessage/IDAOSentMessage.java
@@ -0,0 +1,6 @@
+package Persistence.M08_SendMessage;
+
+import Persistence.IDAO;
+
+public interface IDAOSentMessage extends IDAO {
+}
