f0b6c55468c0148b372d6f5dc39824cfda872cda
marcolbx <marcolbx@hotmail.com>
2019-01-07 11:59:15 -0400

    Cambiado string de currentDate a Date de currentDate en varias clases
    Entity
    DTOTemplate

--- CoreMensajeria/LogicLayer/src/main/java/DTO/M07_Template/DTOTemplate.java
+++ CoreMensajeria/LogicLayer/src/main/java/DTO/M07_Template/DTOTemplate.java
@@ -10,11 +10,12 @@ import Entities.M07_Template.PlanningPackage.Planning;
 import Entities.M07_Template.StatusPackage.Status;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 public class DTOTemplate extends DTO {
 
     private Message _tMessage;
-    private String _tCreationDate;
+    private Date _tCreationDate;
     private int _tTemplateid;
     private Status status;
     private ArrayList<Channel> _tChannels;
@@ -26,7 +27,7 @@ public class DTOTemplate extends DTO {
     public DTOTemplate() {
     }
 
-    public DTOTemplate(Message _tMessage, String _tCreationDate, int _tTemplateid, Status status, ArrayList<Channel> _tChannels, Campaign _tCampaign, Application _tApplication, User _tUser, Planning _tPlanning) {
+    public DTOTemplate(Message _tMessage, Date _tCreationDate, int _tTemplateid, Status status, ArrayList<Channel> _tChannels, Campaign _tCampaign, Application _tApplication, User _tUser, Planning _tPlanning) {
         this._tMessage = _tMessage;
         this._tCreationDate = _tCreationDate;
         this._tTemplateid = _tTemplateid;
@@ -46,11 +47,11 @@ public class DTOTemplate extends DTO {
         this._tMessage = _tMessage;
     }
 
-    public String get_tCreationDate() {
+    public Date get_tCreationDate() {
         return _tCreationDate;
     }
 
-    public void set_tCreationDate(String _tCreationDate) {
+    public void set_tCreationDate(Date _tCreationDate) {
         this._tCreationDate = _tCreationDate;
     }
 
