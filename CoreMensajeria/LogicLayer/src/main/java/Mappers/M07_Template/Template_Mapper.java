a37fb7bde5783a1c828198c7ff6484e649791768
marcolbx <marcolbx@hotmail.com>
2019-01-07 11:51:42 -0400

    Update Template_Mapper.java

--- CoreMensajeria/LogicLayer/src/main/java/Mappers/M07_Template/Template_Mapper.java
+++ CoreMensajeria/LogicLayer/src/main/java/Mappers/M07_Template/Template_Mapper.java
@@ -1,29 +1,54 @@
 package Mappers.M07_Template;
 
+import DTO.DTOFactory;
+import DTO.M07_Template.DTOTemplate;
 import Entities.Entity;
+import Entities.EntityFactory;
+import Entities.M07_Template.Template;
 import Mappers.GenericMapper;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Template_Mapper extends GenericMapper {
 
     @Override
     public Object CreateDto(Entity entity) {
-        return null;
+        try{
+            Template template = (Template) entity;
+            return DTOFactory.CreateDTOTemplate(template.getMessage(),template.getCreationDate(),template.get_id(),template.getStatus(),template.getChannels(),template.getCampaign(),template.getApplication(),template.getUser(),template.getPlanning());
+        }
+        catch (NullPointerException e) {
+            throw e;
+        }
+        catch (Exception e) {
+            throw e;
+        }
     }
 
     @Override
     public Entity CreateEntity(Object o) {
-        return null;
+        DTOTemplate dtoTemplate = (DTOTemplate) o;
+        return EntityFactory.CreateTemplate(dtoTemplate.get_tTemplateid(),dtoTemplate.get_tMessage(),dtoTemplate.get_tCreationDate(),dtoTemplate.get_tChannels(),dtoTemplate.get_tCampaign(),dtoTemplate.get_tApplication(),dtoTemplate.get_tUser(),dtoTemplate.get_tPlanning());
+
     }
 
     @Override
     public List<Entity> CreateEntityList(List list) {
-        return null;
+        ArrayList<Entity> entityList = new ArrayList<Entity>();
+        for (Object e:list){
+            entityList.add(CreateEntity(e));
+        }
+        return entityList;
+
     }
 
     @Override
     public List CreateDtoList(List list) {
-        return null;
+        ArrayList<Object> dtoList = new ArrayList<Object>();
+        for(Object e: list){
+            dtoList.add(CreateDto((Entity) e));
+        }
+        return dtoList;
     }
 }
