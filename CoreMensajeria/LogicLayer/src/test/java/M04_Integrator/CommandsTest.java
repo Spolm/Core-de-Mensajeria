a8d703222c964b122701847371a73d187d9111c3
Braulio <brauliopicon96@gmail.com>
2019-01-08 15:46:40 -0400

    Pruebas Unitarias Arregladas (M04_Integrator)
    Command
    DAOIntegrator

--- CoreMensajeria/LogicLayer/src/test/java/M04_Integrator/CommandsTest.java
+++ CoreMensajeria/LogicLayer/src/test/java/M04_Integrator/CommandsTest.java
@@ -13,25 +13,22 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class CommandsTest {
 
-    private static CommandDisableIntegrator _commandd;
-    private static CommandEnableIntegrator _commande;
+
     private static CommandGetAllIntegrator _commandga;
-    private static CommandGetConcreteIntegrator _commandgci;
-    private static CommandGetIntegratorByChannel _commandgic;
+
 
     @BeforeEach
     public void init() {
-        _commandd = new CommandDisableIntegrator(1);
-        _commande = new CommandEnableIntegrator(1);
+
         _commandga = new CommandGetAllIntegrator();
-        _commandgci = new CommandGetConcreteIntegrator(1);
     }
 
     @Test
     public void commandDisableIntegratorTest() {
         try {
-            _commandd.execute();
-            assertNotNull(_commandd.Return());
+            CommandDisableIntegrator command = new CommandDisableIntegrator(1);
+            command.execute();
+            assertNotNull(command.Return());
         } catch (IntegratorNotFoundException e) {
             e.printStackTrace();
         } catch (DatabaseConnectionProblemException e) {
@@ -43,8 +40,9 @@ public class CommandsTest {
     @Test
     public void commandEnableIntegratorTest() {
         try {
-            _commande.execute();
-            assertNotNull(_commande.Return());
+            CommandEnableIntegrator command = new CommandEnableIntegrator(1);
+            command.execute();
+            assertNotNull(command.Return());
         } catch (IntegratorNotFoundException e) {
             e.printStackTrace();
         } catch (DatabaseConnectionProblemException e) {
@@ -65,9 +63,9 @@ public class CommandsTest {
     @Test
     public void commandGetConcreteIntegrator() {
         try {
-            _commandgci.execute();
-            assertNull(_commandgic.ReturnList());
-            assertNotNull(_commandgci.Return());
+            CommandGetConcreteIntegrator command = new CommandGetConcreteIntegrator(1);
+            command.execute();
+            assertNotNull(command.Return());
         } catch (IntegratorNotFoundException e) {
             e.printStackTrace();
         } catch (DatabaseConnectionProblemException e) {
@@ -79,9 +77,10 @@ public class CommandsTest {
     public void commandGetIntegratorByChannel() {
 
         try {
-            _commandgic.execute();
-            assertNull(_commandgic.Return());
-            assertNotNull(_commandgic.ReturnList());
+            CommandGetIntegratorByChannel command = new CommandGetIntegratorByChannel(1);
+            command.execute();
+            assertNull(command.Return());
+            assertNotNull(command.ReturnList());
         } catch (DatabaseConnectionProblemException e) {
             e.printStackTrace();
         } catch (ChannelNotFoundException e) {
