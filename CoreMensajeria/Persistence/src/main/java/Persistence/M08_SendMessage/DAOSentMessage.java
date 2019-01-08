5454f817b457bf064662717a1900130664622b8c
Fernando Consalvo <fconsalvo97@gmail.com>
2019-01-08 16:07:21 -0400

    Se creo el metodo create

--- CoreMensajeria/Persistence/src/main/java/Persistence/M08_SendMessage/DAOSentMessage.java
+++ CoreMensajeria/Persistence/src/main/java/Persistence/M08_SendMessage/DAOSentMessage.java
@@ -1,10 +1,36 @@
 package Persistence.M08_SendMessage;
 
 import Entities.Entity;
+import Entities.M08_Validation.SentMessage;
+import Persistence.DAO;
 
-public class DAOSentMessage implements IDAOSentMessage{
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DAOSentMessage extends DAO implements IDAOSentMessage{
+    private Connection _conn;
+    final String CALL_INSERT = "{CALL m08_insertSentMessagge(?,?,?,?,?,?)}";
     @Override
     public void create(Entity e) {
+        _conn = getBdConnect();
+        SentMessage sentMessage = (SentMessage) e;
+        try {
+            PreparedStatement preparedStatement = _conn.prepareCall(CALL_INSERT);
+            preparedStatement.setTimestamp(1, sentMessage._sentTime);
+            preparedStatement.setInt(2, sentMessage._messageId);
+            preparedStatement.setInt(3, sentMessage._campaignId);
+            preparedStatement.setInt(4, sentMessage._channelId);
+            preparedStatement.setInt(5, sentMessage._integratorId);
+            preparedStatement.setInt(6, sentMessage._applicationId);
+            ResultSet result = preparedStatement.executeQuery();
+            while (result.next()) {
+                e.set_id(result.getInt("id"));
+            }
+        } catch(SQLException error) {
+
+        }
 
     }
 
