875b8de90e0a9fb94b4c8134f71012d07e5860d5
Sagg_301 <sagg_301@outlook.com>
2019-01-09 06:05:00 -0400

    Cambio en pruebas

--- CoreMensajeria/Persistence/src/test/java/M07_Template/TemplateDAOTest.java
+++ CoreMensajeria/Persistence/src/test/java/M07_Template/TemplateDAOTest.java
@@ -37,7 +37,63 @@ class TemplateDAOTest {
 
     @BeforeAll
     public static void init(){
+        json = "{\n" +
+                "    \"campaign\": 1,\n" +
+                "    \"applicationId\": 1,\n" +
+                "    \"userId\": 1,\n" +
+                "    \"newParameters\": [\"New 52\"],\n" +
+                "    \"company\": 1,\n" +
+                "    \"parameters\": [\n" +
+                "        \"Monto\"\n" +
+                "    ],\n" +
+                "    \"message\": \"Probando [.$Monto$.]\",\n" +
+                "    \"channel_integrator\": [{\n" +
+                "        \"channel\": {\n" +
+                "            \"idChannel\": 1\n" +
+                "        },\n" +
+                "        \"integrator\": {\n" +
+                "            \"idIntegrator\": 1\n" +
+                "        }\n" +
+                "    }],\n" +
+                "    \"planning\": [\n" +
+                "        \"2019-01-09\",\n" +
+                "        \"2019-01-21\",\n" +
+                "        \"22:22\",\n" +
+                "        \"23:02\"\n" +
+                "    ]\n" +
+                "}";
+        _template = (Template) _daoTemplate.postTemplateData(json);
+
+    }
+
+    @Test
+    public void postTemplateDataTest(){
+        try{
+            Template _templateTest = (Template) _daoTemplate.postTemplateData(json);
+            _daoTemplate.deleteTemplate(_templateTest.get_id());
+            assertNotNull(_templateTest,String.valueOf(_template.get_id()));
+        } catch ( Exception e ) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void postTemplateTest(){
+        try {
+            int _templateId = _daoTemplate.postTemplate(1,1,1);
+            _daoTemplate.deleteTemplate( _templateId );
+            assertTrue(_templateId > 0);
+        } catch (Exception e) {
+            e.printStackTrace();
+            //logger.error( "Metodo: {} {}", "getBdConnect", e.toString() );
+        }
+    }
+
+    @Test
+    public void updateTemplateDataTest(){
+        try{
             json = "{\n" +
+                    "    \"templateId\": "+ _template.get_id() +",\n" +
                     "    \"campaign\": 1,\n" +
                     "    \"applicationId\": 1,\n" +
                     "    \"userId\": 1,\n" +
@@ -62,64 +118,7 @@ class TemplateDAOTest {
                     "        \"23:02\"\n" +
                     "    ]\n" +
                     "}";
-
-    }
-
-    @Test
-    public void postTemplateDataTest(){
-        try{
-            _template = (Template) _daoTemplate.postTemplateData(json);
-            _daoTemplate.deleteTemplate(_template.get_id());
-            assertNotNull(_template,String.valueOf(_template.get_id()));
-        } catch ( Exception e ) {
-            e.printStackTrace();
-        }
-    }
-
-    @Test
-    public void postTemplateTest(){
-        try {
-            int _templateId = _daoTemplate.postTemplate(1,1,1);
-            _daoTemplate.deleteTemplate( _templateId );
-            assertTrue(_templateId > 0);
-        } catch (Exception e) {
-            e.printStackTrace();
-            //logger.error( "Metodo: {} {}", "getBdConnect", e.toString() );
-        }
-    }
-
-    @Test
-    public void updateTemplateDataTest(){
-        try{
-            _template = (Template) _daoTemplate.postTemplateData(json);
-            json = "{\n" +
-                    "    \"templateId\": "+_template.get_id()+",\n" +
-                    "    \"campaign\": 1,\n" +
-                    "    \"applicationId\": 1,\n" +
-                    "    \"userId\": 1,\n" +
-                    "    \"newParameters\": [\"New 52\"],\n" +
-                    "    \"company\": 1,\n" +
-                    "    \"parameters\": [\n" +
-                    "        \"Monto\"\n" +
-                    "    ],\n" +
-                    "    \"message\": \"Probando Edicion [.$Monto$.]\",\n" +
-                    "    \"channel_integrator\": [{\n" +
-                    "        \"channel\": {\n" +
-                    "            \"idChannel\": 1\n" +
-                    "        },\n" +
-                    "        \"integrator\": {\n" +
-                    "            \"idIntegrator\": 1\n" +
-                    "        }\n" +
-                    "    }],\n" +
-                    "    \"planning\": [\n" +
-                    "        \"2019-01-09\",\n" +
-                    "        \"2019-01-21\",\n" +
-                    "        \"22:22\",\n" +
-                    "        \"23:02\"\n" +
-                    "    ]\n" +
-                    "}";
             boolean success = DAOFactory.instaciateDaoTemplate().updateTemplateData(json);
-            _daoTemplate.deleteTemplate(_template.get_id());
             assertTrue(success);
         } catch ( Exception e ) {
             e.printStackTrace();
@@ -129,10 +128,8 @@ class TemplateDAOTest {
     @Test
     public void updateTemplateTest(){
         try {
-            _template = (Template) _daoTemplate.postTemplateData(json);
             _daoTemplate.updateTemplate(2,1,_template.get_id());
             _template = (Template)_daoTemplate.get(_template.get_id());
-            _daoTemplate.deleteTemplate(_template.get_id());
 
             assertEquals(2,_template.getCampaign().get_idCampaign());
         } catch (Exception e) {
@@ -144,9 +141,7 @@ class TemplateDAOTest {
     @Test
     public void getTest(){
         try {
-            _template = (Template) _daoTemplate.postTemplateData(json);
             Template _t = (Template) _daoTemplate.get(_template.get_id());
-            _daoTemplate.deleteTemplate(_template.get_id());
             assertNotNull(_t);
         } catch (MessageDoesntExistsException e) {
             e.printStackTrace();
@@ -174,9 +169,7 @@ class TemplateDAOTest {
     @Test
     public void getAllTest(){
         try {
-            _template = (Template) _daoTemplate.postTemplateData(json);
             ArrayList<Entity> list = DAOFactory.instaciateDaoTemplate().getAll();
-            _daoTemplate.deleteTemplate(_template.get_id());
             assertTrue(list.size()>=1);
         } catch (MessageDoesntExistsException e) {
             e.printStackTrace();
@@ -191,9 +184,7 @@ class TemplateDAOTest {
     @Test
     public void getCampaignByTemplateTest(){
         try {
-            _template = (Template) _daoTemplate.postTemplateData(json);
             Campaign _c = (Campaign) _daoTemplate.getCampaignByTemplate(_template.get_id());
-            _daoTemplate.deleteTemplate(_template.get_id());
             assertEquals(1,_c.get_idCampaign());
         } catch (Exception e) {
             e.printStackTrace();
@@ -204,9 +195,7 @@ class TemplateDAOTest {
     @Test
     public void getTemplatesByCampaignTest(){
         try {
-            _template = (Template) _daoTemplate.postTemplateData(json);
             ArrayList<Template> _t = _daoTemplate.getTemplatesByCampaign(3, 1);
-            _daoTemplate.deleteTemplate(_template.get_id());
             assertTrue(_t.size()>=1);
         } catch (Exception e) {
             e.printStackTrace();
@@ -217,9 +206,7 @@ class TemplateDAOTest {
     @Test
     public void getApplicationByTemplateTest(){
         try {
-            _template = (Template) _daoTemplate.postTemplateData(json);
             Application _app = _daoTemplate.getApplicationByTemplate(_template.get_id());
-            _daoTemplate.deleteTemplate(_template.get_id());
             assertEquals(1,_app.get_idApplication());
         } catch (Exception e) {
             e.printStackTrace();
@@ -230,9 +217,7 @@ class TemplateDAOTest {
     @Test
     public void getChannelsByTemplateTest(){
         try {
-            _template = (Template) _daoTemplate.postTemplateData(json);
             ArrayList<Channel> _c = _daoTemplate.getChannelsByTemplate(_template.get_id());
-            _daoTemplate.deleteTemplate(_template.get_id());
             assertTrue(_c.size() > 0);
         } catch (Exception e) {
             e.printStackTrace();
@@ -243,9 +228,7 @@ class TemplateDAOTest {
     @Test
     public void getTemplatePrivilegesByUserTest(){
         try {
-            _template = (Template) _daoTemplate.postTemplateData(json);
             ArrayList<Privilege> _p = _daoTemplate.getTemplatePrivilegesByUser(3,1);
-            _daoTemplate.deleteTemplate(_template.get_id());
             assertTrue(_p.size() > 0);
         } catch (Exception e) {
             e.printStackTrace();
@@ -256,9 +239,7 @@ class TemplateDAOTest {
     @Test
     public void getCampaignsByUserOrCompanyTest(){
         try {
-            _template = (Template) _daoTemplate.postTemplateData(json);
             ArrayList<Campaign> _c = _daoTemplate.getCampaignsByUserOrCompany(3,1);
-            _daoTemplate.deleteTemplate(_template.get_id());
             assertTrue(_c.size() > 0);
         } catch (Exception e) {
             e.printStackTrace();
@@ -266,13 +247,10 @@ class TemplateDAOTest {
         }
     }
 
-    @Test
-    public void postChannelIntegratorTest(){
+    @AfterAll
+    public static void destroy(){
         try {
-            _template = (Template) _daoTemplate.postTemplateData(json);
-            ArrayList<Campaign> _c = _daoTemplate.getCampaignsByUserOrCompany(3,1);
             _daoTemplate.deleteTemplate(_template.get_id());
-            assertTrue(_c.size() > 0);
         } catch (Exception e) {
             e.printStackTrace();
             //logger.error( "Metodo: {} {}", "getBdConnect", e.toString() );
