84ab8c0ad1d051077b38af74c529dcad6fc17f90
Jose Cedeño <jafjdev@gmail.com>
2019-01-10 00:45:21 +0100

    Se agrego mas documentacion a la clase SMSTooLongException
    se agrego mas documentacion a SMSTooLongException

--- CoreMensajeria/Common/src/main/java/Exceptions/M08_SendMessageManager/SMSTooLongException.java
+++ CoreMensajeria/Common/src/main/java/Exceptions/M08_SendMessageManager/SMSTooLongException.java
@@ -3,25 +3,31 @@ package Exceptions.M08_SendMessageManager;
 import Exceptions.PersonalizedException;
 
 /**
- * Excepción personalizada para manejar el tamaño del sms.
+ * Excepción personalizada que se ejecuta cuando
+ * el mensaje es muy largo
+ *
+ * @see PersonalizedException
  */
 public class SMSTooLongException extends PersonalizedException {
 
     /**
      * Constructor vacío.
      */
-    public SMSTooLongException(){
+    public SMSTooLongException() {
         ERROR_CODE = 562;
-        ERROR_MSG = "Ha ocurrido un errror "+ERROR_CODE+" SMS supera 160 caracteres";
+        ERROR_MSG = "Ha ocurrido un errror " + ERROR_CODE + " SMS supera 160 caracteres";
     }
 
     /**
+     * Constructor que recibe la Excepcion por parametro que nos
+     * permite dar mas informacion sobre la excepcion
+     *
      * @param e recibe la excepción que se esta encapsulando.
      */
     public SMSTooLongException(Exception e) {
         super(e);
         ERROR_CODE = 562;
-        ERROR_MSG = "Ha ocurrido un errror "+ERROR_CODE+" SMS supera 160 caracteres";
+        ERROR_MSG = "Ha ocurrido un errror " + ERROR_CODE + " SMS supera 160 caracteres";
 
     }
 
