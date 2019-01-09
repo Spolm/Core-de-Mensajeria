41f2c4189600bfb88805f6b3228959355452b0d8
Spolm <luismachadoz.12@gmail.com>
2019-01-09 19:32:13 -0400

    Arreglando Return en FindByUsernameIdCommand

--- CoreMensajeria/LogicLayer/src/main/java/Logic/M01_Login/FindByUsernameIdCommand.java
+++ CoreMensajeria/LogicLayer/src/main/java/Logic/M01_Login/FindByUsernameIdCommand.java
@@ -1,5 +1,7 @@
 package Logic.M01_Login;
 
+import Entities.Entity;
+import Entities.M01_Login.User;
 import Logic.Command;
 import Persistence.DAOFactory;
 import Persistence.IDAO;
@@ -8,6 +10,7 @@ import Persistence.M01_Login.DAOUser;
 public class FindByUsernameIdCommand extends Command {
 
     private static int _id;
+    private static User _user;
 
     public FindByUsernameIdCommand(int id) {
         _id = id;
@@ -16,11 +19,11 @@ public class FindByUsernameIdCommand extends Command {
     @Override
     public void execute() throws Exception {
         IDAO _dao = DAOFactory.instanciateDaoUser();
-        ((DAOUser) _dao).findByUsernameId(_id);
+        _user = ((DAOUser) _dao).findByUsernameId(_id);
     }
 
     @Override
-    public Object Return() {
-        return _id;
+    public Entity Return() {
+        return _user;
     }
 }
