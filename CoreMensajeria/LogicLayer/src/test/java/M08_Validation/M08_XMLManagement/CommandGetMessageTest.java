0a7588c7be154da18123c940056936619f203720
Unknown <manuel.espinoza1996@gmail.com>
2019-01-09 12:47:07 -0400

    Creando carpetas resource para las pruebas de los xml

--- CoreMensajeria/LogicLayer/src/test/java/M08_Validation/M08_XMLManagement/CommandGetMessageTest.java
+++ CoreMensajeria/LogicLayer/src/test/java/M08_Validation/M08_XMLManagement/CommandGetMessageTest.java
@@ -1,7 +1,27 @@
 package M08_Validation.M08_XMLManagement;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static org.junit.Assert.assertEquals;
 
 class CommandGetMessageTest {
+
+    private static File _file;
+
+    @BeforeEach
+    public void init(){
+        ClassLoader classLoader = new CommandGetMessageTest().getClass().getClassLoader();
+        _file = new File(classLoader.getResource("xml/prueba4.xml").getFile());
+    }
+
+    @Test
+    public void prueba4xml(){
+        System.out.println(_file.getPath());
+        assertEquals("prueba4.xml", _file.getName());
+    }
 
 }
