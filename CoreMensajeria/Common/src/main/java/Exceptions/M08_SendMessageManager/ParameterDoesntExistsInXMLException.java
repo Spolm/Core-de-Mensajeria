67c1693a9c02d967f1dbb94bac56f7c0fc33fbb1
Jose Cedeño <jafjdev@gmail.com>
2019-01-10 00:23:48 +0100

    Se modifico la documentcion de la clase ParameterDoesntExistsInXMLException
    se modifico descripcion de la clase.

--- CoreMensajeria/Common/src/main/java/Exceptions/M08_SendMessageManager/ParameterDoesntExistsInXMLException.java
+++ CoreMensajeria/Common/src/main/java/Exceptions/M08_SendMessageManager/ParameterDoesntExistsInXMLException.java
@@ -3,8 +3,9 @@ package Exceptions.M08_SendMessageManager;
 import Exceptions.PersonalizedException;
 
 /**
- * Excepcion personalizada que se ejecuta cuando los parametros
- * del mensaje no existen
+ * Excepcion personalizada que se ejecuta cuando
+ * el parametro que se recibe en el XML no concuerda
+ * con los parametros de la plantilla.
  *
  * @see PersonalizedException
  */
