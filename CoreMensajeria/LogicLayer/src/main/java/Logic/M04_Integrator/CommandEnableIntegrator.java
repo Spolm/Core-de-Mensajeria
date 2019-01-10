e9579982f5bf4eda651149521d1511a5a4732e0f
Braulio <brauliopicon96@gmail.com>
2019-01-09 20:52:53 -0400

    Agregagacion de Logger en Modulos de integradores y canales
    Se agrego los logger correspondientes en todas aquellas clases que lo requirieron:
    Mapper's
    DAO´s
    Command's

--- CoreMensajeria/LogicLayer/src/main/java/Logic/M04_Integrator/CommandEnableIntegrator.java
+++ CoreMensajeria/LogicLayer/src/main/java/Logic/M04_Integrator/CommandEnableIntegrator.java
@@ -6,18 +6,27 @@ import Exceptions.M04_Integrator.IntegratorNotFoundException;
 import Logic.Command;
 import Persistence.DAOFactory;
 import Persistence.M04_Integrator.IDAOIntegrator;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class CommandEnableIntegrator extends Command {
     private Entity _entity;
     private int _id;
+    final static Logger log = LogManager.getLogger("CoreMensajeria");
 
     public CommandEnableIntegrator(int id) {
+        //region Instrumentation Debug
+        log.debug("Entrando al constructor CommandEnableIntegrator(" + id +")");
+        //endregion
         this._id = id;
         this._entity = null;
     }
 
     @Override
     public void execute() throws IntegratorNotFoundException, DatabaseConnectionProblemException {
+        //region Instrumentation Debug
+        log.debug("Entrando al metodo execute CommandEnableIntegrator");
+        //endregion
         IDAOIntegrator dao = DAOFactory.instanciateDaoIntegrator();
         _entity = dao.enableIntegrator(this._id);
     }
